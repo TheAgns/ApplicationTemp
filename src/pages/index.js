2973472ddb200c9d70560a016a657d7781956cc9
Theagns <markus.agnsgaard@hotmail.com>
2023-04-22 12:12:52 +0200

    more

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
@@ -84,10 +84,7 @@ export default function Home() {
             <h2 className={inter.className}>
               Fourth <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
+            <p className={inter.className}></p>
           </a>
         </div>
       </main>
